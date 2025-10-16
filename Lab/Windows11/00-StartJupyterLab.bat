73336e81e22e161644925cf1fe0255b49fdb7884
Kazutaka Sakoda <kazutaka.sakoda@gmail.com>
2025-10-16 15:07:47 +0900

    StartJupyterLab.bat

--- Lab/Windows11/00-StartJupyterLab.bat
+++ Lab/Windows11/00-StartJupyterLab.bat
@@ -3,7 +3,7 @@ pushd %~dp0
 
 net session >nul 2>&1
 if %errorlevel% neq 0 (
-    echo reraunch with admin privilages... 
+    echo relaunch with admin privilages... 
     powershell -Command "Start-Process '%~f0' -Verb runAs"
     exit /b
 )
